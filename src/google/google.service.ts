876f91290d8e56bf5e199d25d8c0380500ac32f8
hhchoi <choi902307@gmail.com>
2024-12-27 11:47:17 +0900

    feat: gitignore

--- src/google/google.service.ts
+++ src/google/google.service.ts
@@ -1,13 +1,38 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, OnModuleInit } from '@nestjs/common';
 import { SpeechClient } from '@google-cloud/speech';
+import * as fs from 'fs';
+import * as path from 'path';
 
 @Injectable()
-export class GoogleService {
+export class GoogleService implements OnModuleInit {
   private speechClient: SpeechClient;
+  private jsonFilePath: string;
 
+  onModuleInit() {
+    const googlekeyjson = process.env.GOOGLE_CLOUD_KEY_JSON;
+    if (!googlekeyjson) {
+      throw new Error(
+        'GOOGLE_CLOUD_KEY_JSON environment variable is not defined',
+      );
+    }
+
+    // JSON 파일을 저장할 디렉토리 경로 설정
+    const dirPath = 'key';
+
+    // 디렉토리 생성 (존재하지 않을 경우에만)
+    if (!fs.existsSync(dirPath)) {
+      fs.mkdirSync(dirPath, { recursive: true });
+    }
+
+    // JSON 파일 경로 설정
+    this.jsonFilePath = path.join(dirPath, 'secret.json');
+
+    fs.writeFileSync(this.jsonFilePath, googlekeyjson, { encoding: 'utf8' });
+    console.log(`JSON file restored at: ${this.jsonFilePath}`);
+  }
   constructor() {
     this.speechClient = new SpeechClient({
-      keyFilename: './key.json',
+      keyFilename: './key/secret.json',
     });
   }
 
